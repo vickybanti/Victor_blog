43944c22e131cea78d3125586db03bac07b5971b
Vbanti <olamuyiwavictor55@gmail.com>
2022-12-26 20:09:35 +0100

    changed sqlite to postgres

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import backref
+import os
 
 Base = declarative_base()
 def create_app():
@@ -21,13 +22,12 @@ def create_app():
     return app
 
 app = create_app()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 app.app_context().push()
-
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
