4a66c79804cd054a4038994f33f4a8a1f33396d0
Vbanti <olamuyiwavictor55@gmail.com>
2022-12-31 10:22:01 +0100

    changed sqlite to postgres

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ app = create_app()
 ckeditor = CKEditor(app)
 Bootstrap(app)
 app.app_context().push()
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
